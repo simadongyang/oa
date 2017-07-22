dd6caaa2c0406403387b309086e7e927a2d696f6
jie <1090507263@qq.com>
2017-07-22 09:58:31 +0800

    111

--- wwwroot/Application/Common/Conf/config.php
+++ wwwroot/Application/Common/Conf/config.php
@@ -39,10 +39,10 @@ return array(
 
     /* 数据库配置 */
     'DB_TYPE'   => 'mysqli', // 数据库类型
-    'DB_HOST'   => '127.0.0.1', // 服务器地址
-    'DB_NAME'   => 'onethink', // 数据库名
+    'DB_HOST'   => '47.94.43.168', // 服务器地址
+    'DB_NAME'   => 'oa', // 数据库名
     'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => '',  // 密码
+    'DB_PWD'    => 'jp@)!&',  // 密码
     'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => 'onethink_', // 数据库表前缀
 
