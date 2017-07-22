91d3ddc8657351b399324e4fcf1a3b3581c3681e
zhang <18888830442@163.com>
2017-07-22 10:11:04 +0800

    test

--- wwwroot/Application/Common/Conf/config.php
+++ wwwroot/Application/Common/Conf/config.php
@@ -19,7 +19,7 @@ return array(
     //'MODULE_ALLOW_LIST'  => array('Home','Admin'),
 
     /* 系统数据加密设置 */
-    'DATA_AUTH_KEY' => 'LmCx:2E+z[!kD"1N>U~3cB{f9WSqZt]Tl/V&PM|0', //默认数据加密KEY
+    'DATA_AUTH_KEY' => '&^0UfMQou_q[pK<G%({|>6xD#ZNwRJ.9"stk5Ov@', //默认数据加密KEY
 
     /* 调试配置 */
     'SHOW_PAGE_TRACE' => true,
@@ -39,10 +39,10 @@ return array(
 
     /* 数据库配置 */
     'DB_TYPE'   => 'mysqli', // 数据库类型
-    'DB_HOST'   => '127.0.0.1', // 服务器地址
-    'DB_NAME'   => 'onethink', // 数据库名
+    'DB_HOST'   => '47.94.43.168', // 服务器地址
+    'DB_NAME'   => 'oa', // 数据库名
     'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => '',  // 密码
+    'DB_PWD'    => 'jp@)!&',  // 密码
     'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => 'onethink_', // 数据库表前缀
 
