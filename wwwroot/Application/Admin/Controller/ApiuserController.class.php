370c1654ac2be0d554e615c050c94473efb6ee0d
gao <1974898626@qq.com>
2017-07-25 14:55:45 +0800

    ww

--- wwwroot/Application/Admin/Controller/ApiuserController.class.php
+++ wwwroot/Application/Admin/Controller/ApiuserController.class.php
@@ -1,21 +1,21 @@
 <?php
 
 namespace Admin\Controller;
-use User\Api\UserApi;
-use Think\Controller;
 
-class AipConroller extends AdminController{
+
+class AipuserController extends AdminController{
 	//该接口用于查询用户名
 	public function membername(){
+		echo 11;
 		//获取传过来的员工名字
-		$name=I('get.name');
-
-		$list=M('member')->field('uid,realname')->where('like %'.$name.'%')->select();
+		/*$name=I('get.name');
+		$where['realname']=array('like','%'.$name.'%');
+		$list=M('member')->field('uid,realname')->where($where)->select();
 		if($list){	
 			die(json_encode($list));
 		}else{
 			$arr=array('error'=>'不存在满足条件的员工');
 			die(json_encode($arr));
-		}
+		}*/
 	}
 } 
