25389e58755bf42096332531f6cd517aad3f14f1
zhang <18888830442@163.com>
2017-07-26 07:17:14 +0800

    all

--- ganen/Application/User/Conf/config.php
+++ ganen/Application/User/Conf/config.php
@@ -15,5 +15,5 @@
 define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', '-"m)k~oA%J}Y:NutE;v1K^`6DX[BcgP?#_Lql7Hn'); //加密KEY
-define('UC_DB_DSN', 'mysqli://root:jp@)!&@127.0.0.1:3306/ganen'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_DB_DSN', 'mysqli://root:jp@)!&@47.94.43.168:3306/ganen'); // 数据库连接，使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', 'ganen_'); // 数据表前缀，使用Model方式调用API必须配置此项
